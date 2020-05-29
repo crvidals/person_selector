414defc5456bf403e58318b39219a47fd84e66ec
Cristian Vidal <crvidals@gmail.com>
2020-05-28 23:23:39 -0400

    Hartos cambios 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
-export const environment = {
+/*export const environment = {
   production: true
+};*/
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCC5oOeJvXFr6b8PXQLOyijKhuNDsbQmcg",
+    authDomain: "personselector-9cf69.firebaseapp.com",
+    databaseURL: "https://personselector-9cf69.firebaseio.com",
+    projectId: "personselector-9cf69",
+    storageBucket: "personselector-9cf69.appspot.com",
+    messagingSenderId: "28140029213",
+    appId: "1:28140029213:web:19b7b58dac828bc51c0df4",
+    measurementId: "G-SQYSH982VT"
+  }
 };
